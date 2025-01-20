efbe9b2262b996cc465b353b24e63d1db7b969f4
I-am-not-a-Strawberry <morangoassado77@gmail.com>
2025-01-20 15:25:22 -0300

    Funcionalidade para o botão sortear

--- app.js
+++ app.js
@@ -3,25 +3,16 @@
 let amigosLista = [];
 let listaNumero = 0;
 
+
+// Limpa o campo em que escreve o nome do amigo
 function limparCampo() {
     let campo = document.querySelector("input")
     campo.value = ''
 }
 
 
-function getAmigoNome() {
-    let amigoNome = document.querySelector("input").value;
-    if (amigoNome == '' || amigosLista.includes(amigoNome)) {
-        alert("O nome não foi especificado, ou já está presente na lista.")
-    } else {
-        amigosLista.push(amigoNome)
-        console.log(amigosLista)
-        adicionarAmigoListaHTML()
-    }
-    limparCampo()
-}
 
-
+// Mostra o último elemento de amigosLista na lista do HTML
 function adicionarAmigoListaHTML() {
     let minhaListaHTML = document.createElement("li");
     let addLista = document.createTextNode(amigosLista[listaNumero])
@@ -29,5 +20,31 @@ function adicionarAmigoListaHTML() {
     document.getElementById("listaAmigos").appendChild(minhaListaHTML)
     listaNumero++
 }
+
+// Pega o nome escrito e coloca em amigosLista
+
+function getAmigoNome() {
+    let amigoNome = document.querySelector("input").value;
+    // Impede de colocar um nome que não existe
+    // ou um que já foi colocado
+    if (amigoNome == '' || amigosLista.includes(amigoNome)) {
+        alert("O nome não foi especificado, ou já está presente na lista.")
+    } else {
+        amigosLista.push(amigoNome)
+        console.log(amigosLista)
+        adicionarAmigoListaHTML()
+        document.getElementById("sortear").removeAttribute("disabled")
+    }
+    limparCampo()
+}
+
+function sortearAmigo() {
+    let listaIndex = parseInt((Math.random()) * amigosLista.length)
+    let amigoSorteadoTemp = amigosLista[listaIndex]
+    let amigoSorteado = document.getElementById("resultado")
+    amigoSorteado.innerHTML = `${amigoSorteadoTemp} foi sorteado(a)!`
+    console.log(amigosLista[listaIndex])
+}
+
 
 
