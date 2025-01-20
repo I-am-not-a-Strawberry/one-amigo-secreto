7359420d6c4e3bd0de8888816b1a183eeb80821e
I-am-not-a-Strawberry <morangoassado77@gmail.com>
2025-01-20 11:15:45 -0300

    Adicionar funções getAmigoNome e limparCampo

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
-console.log("Hello world!")
+let amigosLista = [];
+
+
+function limparCampo() {
+    let campo = document.querySelector("input")
+    campo.value = ''
+}
+
+
+function getAmigoNome() {
+    let amigoNome = document.querySelector("input").value;
+    if (amigoNome == '' || amigosLista.includes(amigoNome)) {
+        alert("O nome não foi especificado, ou já está presente na lista.")
+    } else {
+        amigosLista.push(amigoNome)
+        console.log(amigosLista)
+    }
+    limparCampo()
+}
+
+
